9ca05ea772e6a22e0ed4987d72fa0ad283eef764
TomasVondra <83216600+TomasVondra@users.noreply.github.com>
2023-03-20 16:38:15 +0100

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Movie(models.Model):
-    name = models.CharField(max_length=200)
-    year = models.IntegerField()
-    description = models.TextField()
-
-def _str_(self):
-    return f"{self.name} ({self.year})"
